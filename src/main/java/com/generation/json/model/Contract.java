7fc7f71a41de3214e6118021c9a6b62ce9483754
Nathan Gabriel <nathancoelho.job@gmail.com>
2023-11-02 14:37:59 -0300

    Create object for store json

--- src/main/java/com/generation/json/model/Contract.java
+++ src/main/java/com/generation/json/model/Contract.java
@@ -1,5 +1,13 @@
 package com.generation.json.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Contract {
+	
+	@JsonProperty("nome")
+	private String nome;
+	
+	@JsonProperty("idade")
+	private int idade;
 
 }
